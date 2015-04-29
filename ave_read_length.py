cc3b30165327dae3e514d52e36b2fd10f803835e
Anatoly Dryga <anatolydryga@gmail.com>
2015-04-29 19:25:48 -0400

    average length of the sequence in the fasta file

--- ave_read_length.py
+++ ave_read_length.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python
+
+import argparse
 from Bio import SeqIO
 
-def average_read_length(filename):
-    handle = open(filename)
-    read_length = []
-    for record in SeqIO.parse(handle, "fasta"):
-        read_length.append(len(record))
-    return read_length
+def average_read_length(fasta_fh):
+    read_length = get_sequence_length(fasta_fh)
+    return sum(read_length)/float(len(read_length))
+
+def get_sequence_length(fasta_fh):
+    seq_length = []
+    for record in SeqIO.parse(fasta_fh, "fasta"):
+        seq_length.append(len(record))
+    return seq_length
 
 if __name__=="__main__":
-    parser = argparse.parser("Average read length for fasta files")
-    parser.add_argument()
-
-    args = parser.parse()
+    parser = argparse.ArgumentParser("Average read length for fasta files")
+    parser.add_argument('fasta_files', nargs='+', type=argparse.FileType('r'))
+    args = parser.parse_args()
 
     for fasta in args.fasta_files:
-        average_read_length = average_read_length(fasta)
-        print fasta + "\t" + str(average_read_length)
+        average = average_read_length(fasta)
+        print fasta.name + "\t" + str(average)
