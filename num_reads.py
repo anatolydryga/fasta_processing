407e950ff373d44615d47f932638ebac8c4a673b
Anatoly Dryga <anatolydryga@gmail.com>
2015-04-29 16:43:26 -0400

    implmentation for multiple files

--- num_reads.py
+++ num_reads.py
@@ -1,19 +1,19 @@
+#!/usr/bin/python
+
+import argparse
+
 from Bio import SeqIO
 
 def number_of_reads(filename):
-    handle = open(filename)
     number_of_reads = 0
-    for record in SeqIO.parse(handle, "fasta"):
+    for record in SeqIO.parse(filename, "fasta"):
         number_of_reads += 1
-    handle.close()
     return number_of_reads
 
 if __name__=="__main__":
-    parser = argparse.parser("Number of reads for fasta files")
-    parser.add_argument()
-
-    args = parser.parse()
+    parser = argparse.ArgumentParser("Number of reads for fasta files")
+    parser.add_argument('fasta_files', nargs='+', type=argparse.FileType('r'))
+    args = parser.parse_args()
 
     for fasta in args.fasta_files:
-        number_of_reads = number_of_reads(fasta)
-        print fasta + "\t" + str(number_of_reads)
+        print fasta.name + "\t" + str(number_of_reads(fasta))
